0e2dea72b13f92a9d6b54953d75d69e31ffc3545
paulinho.borges <paulinhodeveloper@outlook.com>
2025-05-17 11:44:14 -0300

    Inclusão do Spring Initializr

---
+++ src/test/java/br/org/sesisenai/ReciclaVille/ReciclaVilleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.sesisenai.ReciclaVille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclaVilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
